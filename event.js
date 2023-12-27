3d905115623026d974b6155be3a3cca4be48303a
feysalabdi <feisalhaajiaxmed@gmail.com>
2023-12-26 21:19:38 -0500

    first commit

---
+++ event.js
@@ -0,0 +1,17 @@
+let burger= document.querySelector(".burger");
+let navigationBar= document.querySelector(".navigation-bar");
+
+burger.addEventListener('click', ()=>{
+    navigationBar.classList.toggle('active');{
+        
+    } 
+    
+// smooth scroller
+
+
+});   
+
+var scroll = new SmoothScroll('a[href*="#"]',{
+    speed: 1000,
+    speedDuration: true
+});  
